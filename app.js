9f8f4561b522b86f3e84a9e8e06f7ce28fe6bb77
Talha <atercan0658@gmail.com>
2022-09-19 00:29:40 +0200

    overflow fixed

--- app.js
+++ app.js
@@ -13,24 +13,28 @@ addBtn.addEventListener("click", () => {
     alert("Please enter date.");
   } else if (taskBox.value === "") {
     alert("No task written.");
+  } else if (taskBox.value === " ") {
+    alert("No task written.");
   } else {
     addToArray();
-    toDoUl.innerHTML += `<li class="p-0">
-    <div class="row tasks">
+    toDoUl.innerHTML += `<li class="p-0 list-item">
+    <div class="row tasks align-items-center">
         <div class="col-1">
         <input type="checkbox" name="task-check" id="task-check">
       </div>
       <div class="col-2 date d-flex align-items-center">
       ${dateInput.value}
       </div>
-      <div class="col-8">
-      ${taskBox.value}
+      <div class="col-8 d-flex align-items-center">
+      <div class="task-out">${taskBox.value}</div>          
       </div>
       <div class="col-1">
         <i class="fa-regular fa-trash-can trash"></i>
       </div>
     </div>
     </li>`;
+    dateInput.value = "";
+    taskBox.value = "";
   }
   document.querySelector(".trash").addEventListener("click", () => {
     console.log("it works");
