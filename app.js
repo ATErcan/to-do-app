c9670267b623531de580c422f7901d979e8a58eb
Talha <atercan0658@gmail.com>
2022-09-19 09:12:18 +0200

    remove function added

--- app.js
+++ app.js
@@ -17,26 +17,31 @@ addBtn.addEventListener("click", () => {
     alert("No task written.");
   } else {
     addToArray();
+    counter += 1;
     toDoUl.innerHTML += `<li class="p-0 list-item">
-    <div class="row tasks align-items-center">
+      <div class="row tasks align-items-center">
         <div class="col-1">
-        <input type="checkbox" name="task-check" id="task-check">
+          <input type="checkbox" name="task-check" id="task-check">
+        </div>
+        <div class="col-2 date d-flex align-items-center">
+          ${dateInput.value}
+        </div>
+        <div class="col-8 d-flex align-items-center">
+          <div class="task-out">${taskBox.value}</div>          
+        </div>
+        <div class="col-1">
+          <i class="fa-regular fa-trash-can trash"></i>
+        </div>
       </div>
-      <div class="col-2 date d-flex align-items-center">
-      ${dateInput.value}
-      </div>
-      <div class="col-8 d-flex align-items-center">
-      <div class="task-out">${taskBox.value}</div>          
-      </div>
-      <div class="col-1">
-        <i class="fa-regular fa-trash-can trash"></i>
-      </div>
-    </div>
     </li>`;
     dateInput.value = "";
     taskBox.value = "";
   }
-  document.querySelector(".trash").addEventListener("click", () => {
-    console.log("it works");
-  });
+
+  const trashBin = document.querySelectorAll(".trash");
+  for (let item of trashBin) {
+    item.addEventListener("click", () => {
+      item.parentElement.parentElement.parentElement.remove();
+    });
+  }
 });
